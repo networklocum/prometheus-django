bce51cfc672a36977b0af1bcf84f470a9832a137
Damilare Onajole <web.dami@gmail.com>
2017-09-29 15:38:34 +0100

    expose prom metrics from django

---
+++ prometheus_django/metrics.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+from django.http import HttpResponse
+from prometheus_client import multiprocess
+from prometheus_client import generate_latest, CollectorRegistry, CONTENT_TYPE_LATEST
+
+
+# Create your views here.
+
+def metrics(request):
+    if 'prometheus_multiproc_dir' in os.environ:
+        registry = CollectorRegistry()
+        multiprocess.MultiProcessCollector(registry)
+        data = generate_latest(registry)
+        return HttpResponse(data, content_type=CONTENT_TYPE_LATEST)
+    return HttpResponse('')

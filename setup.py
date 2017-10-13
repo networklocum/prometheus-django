a9b20563a4ec9fe907752f7a2d77a5a6b280fab0
Javier Rengel <rephus@gmail.com>
2017-10-13 16:07:38 +0200

    Update package

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(
-    name = 'prometheus-django',
+    name = 'prometheus_django',
     version = '1.0',
     description = 'Prometheus django middleware',
     packages = [
         'prometheus-django',
+        'prometheus_django',
     ],
     install_requires = [
         'prometheus_client',

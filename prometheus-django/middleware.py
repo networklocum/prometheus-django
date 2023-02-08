4c186f202c1ff811e18008cb9903f8dd3be1240c
Lex Robinson <lex.robinson@lantum.com>
2023-02-08 14:58:17 +0000

    Delete duplicate prometheus-django.middleware module
    This is still used in an unfortunate number of services so until I get
    around to fixing that we need to keep this silly shim around but we
    don't need to have the duplicate code

--- prometheus-django/middleware.py
+++ prometheus-django/middleware.py
@@ -1,64 +1,16 @@
-import re
-import time
-from prometheus_client import Counter, Summary, Gauge
+"""
+@deprecated
+This module will exist until I've gone round all our codebase and deleted everything
+that references it, then it's getting deleted
+"""
+import warnings
 
-try:
-    # MiddlewareMixin is for backwards compatibility from django >=1.10
-    # to earlier versions, so it didn't exist before django 1.10.
-    # In our older Microservices we don't need to use this.
-    # https://docs.djangoproject.com/en/2.2/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware
-    from django.utils.deprecation import MiddlewareMixin
-except ImportError:
-    MiddlewareMixin = object
+from prometheus_django.middleware import PrometheusMiddleware
 
+warnings.warn(
+    "Use prometheus_django not prometheus-django",
+    DeprecationWarning,
+    stacklevel=2,
+)
 
-def replace_id_in_url(url):
-    """
-        Replace id on url so we can easily group by them
-        > replace_id_in_url(/user/1234/)
-        /user/:id/
-    """
-    return re.sub(r'/[0-9]+/', '/:id/', url)
-
-
-PROMETHEUS_REQUEST_DURATION = Summary('http_request_duration_microseconds',
-                                      'Request duration in microseconds',
-                                      ['method', 'endpoint'])
-PROMETHEUS_REQUEST_TOTAL = Counter('http_requests_total',
-                                   'Request total wiht status',
-                                   ['method', 'endpoint', 'http_status'])
-PROMETHEUS_REQUEST_IN_PROGRESS = Gauge('http_requests_in_progress_total',
-                                       'Requests in progress',
-                                       ['method', 'endpoint'])
-
-
-class PrometheusMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        """
-            Save initial time of the request when it starts
-        """
-        url = replace_id_in_url(request.META.get('PATH_INFO'))
-
-        PROMETHEUS_REQUEST_IN_PROGRESS.labels(
-            request.method, url).inc()
-
-        request.prometheus_start_time = time.time()
-
-    def process_response(self, request, response):
-        """
-            Calculate the request latency by looking at the time we stored
-            at the beggining of the request
-        """
-        if hasattr(request, 'prometheus_start_time'):
-            request_latency = time.time() - request.prometheus_start_time
-            latency_microseconds = request_latency / 1000000
-            url = replace_id_in_url(request.META.get('PATH_INFO'))
-
-            PROMETHEUS_REQUEST_DURATION.labels(
-                request.method, url).observe(latency_microseconds)
-            PROMETHEUS_REQUEST_TOTAL.labels(
-                request.method, url, response.status_code).inc()
-            PROMETHEUS_REQUEST_IN_PROGRESS.labels(
-                request.method, url).dec()
-
-        return response
+__all__ = ["PrometheusMiddleware"]

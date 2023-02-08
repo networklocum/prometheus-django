01a6539c5d3b176306aea8130f3083934efc9042
Lex Robinson <lex.robinson@lantum.com>
2023-02-08 15:12:27 +0000

    Format with Black

--- prometheus_django/metrics.py
+++ prometheus_django/metrics.py
@@ -5,10 +5,11 @@ from django.http import HttpResponse
 from prometheus_client import multiprocess
 from prometheus_client import generate_latest, CollectorRegistry, CONTENT_TYPE_LATEST
 
+
 def metrics(request):
-    if 'prometheus_multiproc_dir' in os.environ:
+    if "prometheus_multiproc_dir" in os.environ:
         registry = CollectorRegistry()
         multiprocess.MultiProcessCollector(registry)
         data = generate_latest(registry)
         return HttpResponse(data, content_type=CONTENT_TYPE_LATEST)
-    return HttpResponse('')
+    return HttpResponse("")
